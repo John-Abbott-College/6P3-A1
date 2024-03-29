267f0c9cd58d2a0a32db2201a122aa65aeed2e8e
GriffinBonomo <griffinbonomo@gmail.com>
2024-03-29 17:03:49 -0400

    Implemented temp_humi sensor

--- temp_humi_sensor.py
+++ temp_humi_sensor.py
@@ -7,15 +7,24 @@ from grove.grove_temperature_humidity_aht20 import GroveTemperatureHumidityAHT20
 from time import sleep
 
 class Sensor(ISensor): 
+    _sensor_model: str
+    reading_type: AReading.Type
+
     def __init__(self, gpio: int, model: str, type: AReading.Type):
-        #self.sensor = GroveTemperatureHumidityAHT20(0x38, 4)
-        print("Yeah I did stuff")
+        self._sensor_model = model
+        self.reading_type = type
+
+        self.sensor = GroveTemperatureHumidityAHT20(0x38, 4)
 
     def read_sensor(self) -> list[AReading]:
-        #return self.sensor.read()
-        print("NOT IMPLEMENTED YET")
+        _temperature, _humidity = 0, 1
+        _reading = self.sensor.read()
 
-sensor = Sensor(1, "H", AReading(AReading.Type.TEMPERATURE, AReading.Unit.CELCIUS, 1))
-temp = sensor.read_sensor()
+        return [AReading(AReading.Type.TEMPERATURE, AReading.Unit.CELCIUS, _reading[_temperature]),
+            AReading(AReading.Type.HUMIDITY, AReading.Unit.HUMIDITY, _reading[_humidity])]
+
+# sensor = Sensor(1, "AH20", AReading(AReading.Type.TEMPERATURE, AReading.Unit.CELCIUS, 1))
+# temp = sensor.read_sensor()
+# print(temp)
 
 
