fd91d5ec8e1b3cc4926b99aa20042d178852b69c
zakarigaudreault <zakarigaudreault@hotmail.com>
2024-04-07 21:21:01 +0100

    testing

--- actuators.py
+++ actuators.py
@@ -1,64 +1,30 @@
-from abc import ABC, abstractmethod
-from enum import Enum
+from gpiozero import OutputDevice
+from time import sleep
+from typing import Union
 
+class FanController:
+    def __init__(self, relay_pin: int):
+        self.relay = OutputDevice(relay_pin)
 
-class ACommand(ABC):
-    """Abstract class for actuator command. Can be instantiated directly or inherited.
-    Also defines all possible command types via enums.
-    """
+    def control_actuator(self, new_state: str) -> bool:
+        if new_state.lower() == "on":
+            self.relay.on()
+            return True
+        elif new_state.lower() == "off":
+            self.relay.off()
+            return True
+        else:
+            return False
+##
+if __name__ == "__main__":
+    relay_pin = 16  
+    fan_controller = FanController(relay_pin)
 
-    class Type(str, Enum):
-        """Enum defining types of actuators that can be targets for a command
-        """
-        FAN = 'fan'
-        LIGHT_ON_OFF = 'light-on-off'
-        LIGHT_PULSE = 'light-pulse'
-
-    # Class properties that must be defined in implementation classes
-
-    def __init__(self, target: Type, value: str) -> None:
-        """Constructor for Command abstract class
-
-        :param Type target: Type of command whih associated a command to a type of actuator.
-        :param str value: Value of command to be passed to actuator.
-        """
-        self.target_type = target
-        self.value: str = value
-
-    def __repr__(self) -> str:
-        return f'Command setting {self.target_type} to {self.value}'
-
-
-class IActuator(ABC):
-
-    # Class properties that must be set in constructor of implementation class
-    _current_state: str
-    type: ACommand.Type
-
-    @abstractmethod
-    def __init__(self, gpio: int, type: ACommand.Type, initial_state: str) -> None:
-        """Constructor for Actuator class. Must define interface's class properties
-
-        :param ACommand.Type type: Type of command the actuator can respond to.
-        :param str initial_state: initializes 'current_state' property of a new actuator.
-        If not passed, actuator implementation is responsible for setting a default value.
-        """
-        pass
-
-    @abstractmethod
-    def validate_command(self, command: ACommand) -> bool:
-        """Validates that a command can be used with the specific actuator.
-
-        :param ACommand command: the command to be validated.
-        :return bool: True if command can be consumed by the actuator.
-        """
-        pass
-
-    @abstractmethod
-    def control_actuator(self, value: str) -> bool:
-        """Sets the actuator to the value passed as argument.
-
-        :param str value: Value used to set the new state of the actuator. Value is parsed inside concrete classes.
-        :return bool: True if the state of the actuator changed, false otherwise.
-        """
-        pass
+    try:
+        while True:
+            fan_controller.control_actuator("on")
+            sleep(2)
+            fan_controller.control_actuator("off")
+            sleep(2)
+    except KeyboardInterrupt:
+        print("Fan control interrupted.")
--- sensors.py
+++ sensors.py
@@ -1,66 +1,36 @@
-from abc import ABC, abstractmethod
-from enum import Enum
+import sys
+sys.path.append("/home/zakarigaudreault/lab4") 
 
+from grove.grove_temperature_humidity_aht20 import GroveTemperatureHumidityAHT20
+import time
+from typing import Union
 
-class AReading(ABC):
-    """Abstract class for sensor readings. Can be instantiated directly or inherited.
-    Also defines all possible types of readings and reading units using enums.
-    """
+class HumiditySensor:
+    def __init__(self, address: hex = 0x38, bus: int = 4) -> None:
+        self.address = address
+        self.bus = bus
+        self.sensor = None
+        try:
+            self.sensor = GroveTemperatureHumidityAHT20(address=address, bus=bus)
+        except OSError as e:
+            print("Error initializing sensor:", e)
+    
+    def read_sensor(self) -> Union[tuple, None]:
+        if self.sensor:
+            try:
+                return self.sensor.read()
+            except OSError as e:
+                print("Error reading sensor:", e)
+                return None
+        else:
+            print("Sensor not initialized.")
+            return None
 
-    class Type(str, Enum):
-        """Enum defining all possible types of readings that sensors might make.
-        """
-        # Add new reading types here.
-        TEMPERATURE = 'temperature'
-        HUMIDITY = 'humidity'
-        LUMINOSITY = 'luminosity'
-
-    class Unit(str, Enum):
-        """Enum defining all possible units for sensor measuremens.
-        """
-        # Add new reading units here.
-        MILLIMITERS = 'mm'
-        CELCIUS = '°C'
-        FAHRENHEIT = '°F'
-        HUMIDITY = '% HR'
-        UNITLESS = 'unitless'
-
-    # Class properties that must be defined in implementation classes
-    reading_type: Type
-    reading_unit: Unit
-    value: float
-
-    def __init__(self, type: Type, unit: Unit, value: float) -> None:
-        self.reading_type = type
-        self.reading_unit = unit
-        self.value = value
-
-    def __repr__(self) -> str:
-        """String representation of a reading object
-        """
-        return f"{self.reading_type}: {self.value} {self.reading_unit}"
-
-
-class ISensor(ABC):
-    """Interface for all sensors.
-    """
-
-    # Class properties that must be defined in implementation classes
-    _sensor_model: str
-    reading_type: AReading.Type
-
-    @abstractmethod
-    def __init__(self, gpio: int,  model: str, type: AReading.Type):
-        """Constructor for Sensor  class. May be called from childclass.
-
-        :param str model: specific model of sensor hardware. Ex. AHT20 or LTR-303ALS-01
-        :param ReadingType type: Type of reading this sensor produces. Ex. 'TEMPERATURE'
-        """
-
-    @abstractmethod
-    def read_sensor(self) -> list[AReading]:
-        """Takes a reading form the sensor
-
-        :return list[AReading]: List of readinds measured by the sensor. Most sensors return a list with a single item.
-        """
-        pass
+if __name__ == "__main__":
+    sensor = HumiditySensor()
+    
+    while True:
+        sensor_reading = sensor.read_sensor()
+        if sensor_reading:
+            print(sensor_reading)
+        time.sleep(2)
