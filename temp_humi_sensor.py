6ac735338f1a3b0a3052879e7d2ad768aa636ba1
breannadefo <2145494@johnabbottcollege.net>
2024-04-07 23:26:06 -0400

    Refactored the temperature and humidity sensor code.

--- temp_humi_sensor.py
+++ temp_humi_sensor.py
@@ -1,21 +1,36 @@
 from grove.grove_temperature_humidity_aht20 import GroveTemperatureHumidityAHT20
+from sensors import ISensor, AReading
 from time import sleep
 
 
-class TempHumiditySensor(ISensor, IActuator): 
-    def __init__(self, address:hex=0x38, bus:int=4) -> None:
-        self.sensor = GroveTemperatureHumidityAHT20(address = address, bus = bus)
+class TempHumiditySensor(ISensor): 
+    def __init__(self, gpio: int,  model: str, type: AReading.Type):
+        """Constructor for Sensor  class. May be called from childclass.
 
-    def read_sensor(self) -> list[float]: 
-        return list(self.sensor.read())
+        :param int gpio: bus number for the sensor
+        :param str model: specific model of sensor hardware. Ex. AHT20 or LTR-303ALS-01
+        :param ReadingType type: Type of reading this sensor produces. Ex. 'TEMPERATURE'
+        """
+        self._sensor_model = model
+        self.reading_type = type
+        self.sensor = GroveTemperatureHumidityAHT20(bus = gpio)
+
+    def read_sensor(self) -> list[AReading]:
+        """Reads the sensor to get the temperature and humidity.
+        """
+        temperature, humidity = self.sensor.read() 
+        return [
+            AReading(AReading.Type.TEMPERATURE, AReading.Unit.CELCIUS, temperature),
+            AReading(AReading.Type.HUMIDITY, AReading.Unit.CELCIUS, humidity)
+        ]
     
 
 def main():
-    sensor = TempHumiditySensor()
+    sensor = TempHumiditySensor(4, "AHT20", AReading.Type.TEMPERATURE)
     while True:
         temperature, humidity = sensor.read_sensor()
-        print('Temperature in Celsius is {:.2f} C'.format(temperature))
-        print('Relative Humidity is {:.2f} %'.format(humidity))
+        print('Temperature in Celsius is {:.2f} C'.format(temperature.value))
+        print('Relative Humidity is {:.2f} %'.format(humidity.value))
         sleep(1)
 
 
