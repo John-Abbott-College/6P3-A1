e2860a3a33aee725a8806fc16ff2d556a992220d
Cindy-Coulibaly <2126537@johnabbottcollege.net>
2024-04-07 23:44:37 -0400

    fan uses IActuator interface

--- fan_control.py
+++ fan_control.py
@@ -1,31 +1,37 @@
+#!/usr/bin/env python
 from gpiozero import OutputDevice
 from time import sleep
+from actuators import IActuator,ACommand
 
 
-class FanActuator:
+class FanActuator(IActuator):
     def __init__(self, gpio: int, initial_state: str = "0") -> None:
         self.gpio = gpio
-        self.current_state = initial_state
+        self._current_state = initial_state
         self.fan = OutputDevice(pin=gpio)
+        self.type = ACommand.Type.FAN
 
 
     def control_actuator(self, value: str) -> bool:
-        previous_state = self.current_state
+        previous_state = self._current_state
         try:
             self.fan.value = int(value)
         except (ValueError, TypeError):
             print(f"Invalid argument {value}, must be 0 or 1")
-        self.current_state = str(self.fan.value)
-        return previous_state != self.current_state
+        self._current_state = str(self.fan.value)
+        return previous_state != self._current_state
+    
+    def validate_command(self, command: ACommand) -> bool:
+        return command.target_type == ACommand.Type.FAN
 
 
 def main():
     fan_actuator = FanActuator(gpio=16)
     fan_actuator.control_actuator("1")
-    print(f"Fan state: {fan_actuator.current_state}")
+    print(f"Fan state: {fan_actuator._current_state}")
     sleep(2)
     fan_actuator.control_actuator("0")
-    print(f"Fan state: {fan_actuator.current_state}")
+    print(f"Fan state: {fan_actuator._current_state}")
     sleep(2)
 
 
