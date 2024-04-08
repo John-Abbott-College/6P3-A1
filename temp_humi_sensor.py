26a0bc330d30efdb673892c8df95d275bfbad098
Breanna <bdeforest@gmail.com>
2024-04-07 22:20:40 -0400

    Add sensor and controller files and update .gitignore

---
+++ temp_humi_sensor.py
@@ -0,0 +1,26 @@
+from grove.grove_temperature_humidity_aht20 import GroveTemperatureHumidityAHT20
+from time import sleep
+
+
+class TempHumiditySensor(ISensor, IActuator): 
+    def __init__(self, address:hex=0x38, bus:int=4) -> None:
+        self.sensor = GroveTemperatureHumidityAHT20(address = address, bus = bus)
+
+    def read_sensor(self) -> list[float]: 
+        return list(self.sensor.read())
+    
+
+def main():
+    sensor = TempHumiditySensor()
+    while True:
+        temperature, humidity = sensor.read_sensor()
+        print('Temperature in Celsius is {:.2f} C'.format(temperature))
+        print('Relative Humidity is {:.2f} %'.format(humidity))
+        sleep(1)
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
