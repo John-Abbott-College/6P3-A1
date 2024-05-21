c8f4746ab919b5bcf1ac7c93759793900bd0bdc9
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 <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>
2024-05-20 23:28:29 -0400

    Fan Control updated

--- fan_control.py
+++ fan_control.py
@@ -1,14 +1,16 @@
 from gpiozero import OutputDevice
 from time import sleep
-from actuators import IActuator
+from actuators import IActuator, ACommand
 
 
-class Fan(IActuator):
+class FanActuator(IActuator):
     def __init__(self, gpio: int, initial_state: str = "0") -> None:
         self.gpio = gpio
         self.current_state = initial_state
         self.fan = OutputDevice(pin=gpio)
 
+    def validate_command(self, command: ACommand) -> bool:
+        return command.target_type == ACommand.Type.FAN
 
     def control_actuator(self, value: str) -> bool:
         previous_state = self.current_state
